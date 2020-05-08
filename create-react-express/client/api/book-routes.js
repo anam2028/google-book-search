e427ec8460fa2a179e3403dae9c58d9224a0c2ee
Anamika Panna <anamika2028@gmail.com>
2020-05-08 15:32:50 -0400

    updated the backend structure

---
+++ create-react-express/client/api/book-routes.js
@@ -0,0 +1,13 @@
+const router = require('express').Router()
+
+const { getSavedBook,saveBook,removeBook } = require('../controllers/book-controller')
+
+//GET and POST at /api/books
+
+router.route('/').get(getSavedBook).post(saveBook)
+
+//DELETE at /api/books/:id
+
+router.route('/:id').delete(removeBook)
+
+module.exports = router;
--- create-react-express/server.js
+++ create-react-express/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const routes = require('./routes');
 // const path = require("path");
 
 const PORT = process.env.PORT || 3001;
@@ -13,7 +14,7 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
-// app.use(routes);
+app.use(routes);
 
 mongoose.Promise = Promise;
 mongoose.connect(
